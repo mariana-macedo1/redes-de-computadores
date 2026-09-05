a7703c7641eb98011b259008043db94067b8e217
Mariana Macedo <marianamacedodefarias@gmail.com>
2026-09-05 14:46:40 -0300

    feat: estrutura inicial da aplicação Django

---
+++ cadastro/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Aluno
+
+def index(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        curso = request.POST.get("curso")
+
+        Aluno.objects.create(nome=nome,curso=curso)
+
+        return redirect("index")
+    
+    alunos  = Aluno.objects.all().order_by("-id")
+
+    return render(
+        request,
+        "cadastro/index.html",
+        {"alunos": alunos}
+)
+
